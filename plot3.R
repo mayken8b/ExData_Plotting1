1f73b4cc19cdbd718ee077a7f10d3e7881881931
mayken8b <mca8b@virginia.edu>
2014-07-13 02:53:18 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+# read in data and clean it
 power_data <- read.table("household_power_consumption.txt", comment.char = "", 
                          sep = ";", header = TRUE, na.strings = "?",
                          colClasses = c("character", "character", "numeric", 
@@ -11,8 +12,10 @@ clean_data$Date <- strptime(paste(clean_data$Date, clean_data$Time),
 
 final_data <- clean_data[,c(1, 3, 4, 5, 6, 7, 8, 9)]
 
+# create graphics png device in working directory
 png(filename = "plot3.png")
 
+# create plots
 plot(final_data$Date, final_data$Sub_metering_1, type = "n", xlab = "",
      ylab = "Energy sub metering")
 lines(final_data$Date, final_data$Sub_metering_1)
@@ -22,4 +25,5 @@ legend("topright", legend = c("Sub_metering_1", "Sub_metering_2",
                               "Sub_metering_3"), lwd = 1,
        col = c("black", "red", "blue"))
 
+# turn off graphics device
 dev.off()
